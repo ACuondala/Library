709880556e2db88b0940dab524b75b9c20bab320
ACuondala <antoniomanuelmendesdacosta@gmail.com>
2025-10-29 16:42:28 +0100

    feat:books

--- src/main/java/com/example/nada/Mappers/BookMapper.java
+++ src/main/java/com/example/nada/Mappers/BookMapper.java
@@ -2,17 +2,23 @@ package com.example.nada.Mappers;
 
 import com.example.nada.Dtos.BookDtos.BookRequestDto;
 import com.example.nada.Dtos.BookDtos.BookResponseDto;
+import com.example.nada.Models.Author;
 import com.example.nada.Models.Book;
 import com.example.nada.Models.Category;
 import org.mapstruct.*;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",
         uses={AuthorMapper.class,CategoryMapper.class, PublisherMapper.class},
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface BookMapper {
+
+
+
 
 
     BookResponseDto toDto(Book book);
@@ -23,9 +29,10 @@ public interface BookMapper {
     Book toEntity(BookRequestDto dto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "publisher", ignore = true)
+    @Mapping(target = "authors", ignore = true)
     void updateBook(BookRequestDto dto, @MappingTarget Book book);
 
-    default Category map(UUID id) {
-        return null;
-    }
+    
 }
